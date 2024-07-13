c2e12e8d63d9e6c49d8234ca1c071e3fa0b2de0e
Pintos Joaquin <joale7546@gmail.com>
2024-07-13 17:23:10 -0300

    Inicializacion del proyecto

---
+++ src/test/java/com/codingdojo/saludGuard/SaludGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.saludGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
