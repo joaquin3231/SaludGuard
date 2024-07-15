b16affa16d018a3190b95f33bdd472038495b648
PC <milagrosnavarro1d@gmail.com>
2024-07-14 22:17:31 -0300

    agregando interface user repository

---
+++ src/main/java/com/codingdojo/saludGuard/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.codingdojo.saludGuard.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.saludGuard.models.User;
+
+@Repository
+
+public interface UserRepository extends CrudRepository<User,Long>{
+	
+	List<User> findAll();
+}
