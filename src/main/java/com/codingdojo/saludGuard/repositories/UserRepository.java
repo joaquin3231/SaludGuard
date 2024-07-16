977f8beb9abe487534a7c60e0b85bb6f46c33da5
PC <milagrosnavarro1d@gmail.com>
2024-07-16 19:08:13 -0300

    SERVICIO PARA REGISTRARSE Y LOGIN USUARIO

--- src/main/java/com/codingdojo/saludGuard/repositories/UserRepository.java
+++ src/main/java/com/codingdojo/saludGuard/repositories/UserRepository.java
@@ -11,5 +11,7 @@ import com.codingdojo.saludGuard.models.User;
 
 public interface UserRepository extends CrudRepository<User,Long>{
 	
+	User findByEmail(String email);
+	
 	List<User> findAll();
 }
--- src/main/java/com/codingdojo/saludGuard/services/UserService.java
+++ src/main/java/com/codingdojo/saludGuard/services/UserService.java
@@ -2,8 +2,10 @@ package com.codingdojo.saludGuard.services;
 
 import java.util.List;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import com.codingdojo.saludGuard.models.User;
 import com.codingdojo.saludGuard.repositories.UserRepository;
@@ -16,4 +18,66 @@ public class UserService {
 	public List<User> findAllUsers(){
 		return ur.findAll();
 	}
+	
+	/*METODO PARA REGISTRAR USUARIO*/
+	
+	public User register(User newUser, BindingResult result) {
+		//COMPARAR CONTRASEÑAS
+		String password = newUser.getPassword();
+		String confirm = newUser.getComfirm();
+		
+		//revisar q el email no este registrado
+		if(!password.equals(confirm)) {
+			
+			result.rejectValue("comfirm", "Matches", "Password and confirmation don't match");
+		}
+	
+		String email= newUser.getEmail();
+		User userExist = ur.findByEmail(email);
+		if(userExist != null) {
+			//el correo ya esta registrado
+			result.rejectValue("email", "Unique", "Email already exists");
+		}
+		
+		//si existe error , regresa null
+		
+		if(result.hasErrors()) {
+			return null;
+		}else {
+			//no hay error
+			//hashear contraseña
+			String passHash = BCrypt.hashpw(password, BCrypt.gensalt());
+			
+			newUser.setPassword(passHash);
+			return ur.save(newUser);
+		}
+	}
+	
+	
+	public User login (String email, String password) {
+		//revisamos que el correo exista en bdd
+		User userTryingLogin = ur.findByEmail(email);
+		
+		if(userTryingLogin == null) {
+			return null;
+		}
+		
+		//comparar las contraseñas
+		if(BCrypt.checkpw(password, userTryingLogin.getPassword())) {
+			return userTryingLogin;
+		}else {
+			return null;
+		}
+		
+		
+	}
+	
+	//METODO QUE GUARDA USUARIO
+	public User saveUser (User user) {
+		return ur.save(user);
+	}
+	
+	
+	
+	
 }
